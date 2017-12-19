f0fa099a4a191ccdbf8776ab7c7e4e4ec51288b0
Unknown <bofmichal@gmail.com>
2017-12-19 21:38:05 +0100

    featzmznb

---
+++ js/proj.js
@@ -0,0 +1,12 @@
+function zaladuj(){
+    var zdjecie = "<img src=\"IMG_1047.jpg\" width=\"120px\"/>";
+    document.getElementById("michal").innerHTML=zdjecie;
+                
+    var urodzenie = "19.06.1987";
+    document.getElementById("data_ur").innerHTML = urodzenie;
+    
+    var adres = "ul. Wesoła 27 m 7, 15-307 Białystok";
+    document.getElementById("adres").innerHTML = adres;
+
+    
+}
